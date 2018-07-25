37c1f6fbc8b0c5f0dc43b2873a3effa794b0727d
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-07-25 20:48:49 -0300

    Add getDevices

--- esm/Client.js
+++ esm/Client.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import meshblu from 'meshblu';
 
 function createConnection(hostname, port, uuid, token) {
@@ -20,6 +21,42 @@ function connect(hostname, port, uuid, token) {
     connection.on('notReady', () => {
       connection.close(() => {});
       reject(new Error('Connection not authorized'));
+    });
+  });
+}
+
+function mapDevice(device) {
+  return _.omit(device, [
+    'uuid',
+    '_id',
+    'owner',
+    'type',
+    'ipAddress',
+    'token',
+    'meshblu',
+    'discoverWhitelist',
+    'configureWhitelist',
+    'socketid',
+    'secure',
+    'get_data',
+    'set_data',
+  ]);
+}
+
+function getDevices(connection) {
+  return new Promise((resolve, reject) => {
+    if (!connection) {
+      reject(new Error('Not connected'));
+      return;
+    }
+
+    connection.devices({ gateways: ['*'] }, (result) => {
+      if (result.error) {
+        reject(result.error);
+        return;
+      }
+
+      resolve(result);
     });
   });
 }
@@ -52,6 +89,11 @@ class Client {
         resolve();
       });
     });
+  }
+
+  async getDevices() {
+    const devices = await getDevices(this.connection);
+    return devices.map(mapDevice);
   }
 }
 
