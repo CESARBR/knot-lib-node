a089604e10831fe69b2762590df86681d705c0a0
Ramon Ribeiro <rhpr@cesar.org.br>
2018-09-04 17:18:00 -0300

    Filter devices returned from getDevices
    Only devices registered should be listed

--- esm/Client.js
+++ esm/Client.js
@@ -62,6 +62,10 @@ function mapDevice(device) {
   ]);
 }
 
+function filterDevice(device) {
+  return device.schema && device.id && device.name;
+}
+
 function getDevices(connection) {
   return new Promise((resolve, reject) => {
     if (!connection) {
@@ -137,7 +141,7 @@ class Client {
 
   async getDevices() {
     const devices = await getDevices(this.connection);
-    return devices.map(mapDevice);
+    return devices.filter(filterDevice).map(mapDevice);
   }
 
   async getDevice(id) {
